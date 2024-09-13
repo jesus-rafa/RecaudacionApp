20e5ddf61da9e5117eab403562810b58b9fd4e85
rlopezro <70593750+jesus-rafa@users.noreply.github.com>
2024-09-13 14:41:57 -0600

    Update local.py

--- Recaudacion/settings/local.py
+++ Recaudacion/settings/local.py
@@ -126,8 +126,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIT_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'rafalopezrl749@gmail.com'
-EMAIL_HOST_PASSWORD = 'R@f@el1995'
+EMAIL_HOST_USER = '*****'
+EMAIL_HOST_PASSWORD = '*****'
 
 
 # ADMINS = (('Admin', 'rafalopezrl749@gmail.com'),)
